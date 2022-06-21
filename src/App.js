23df5b87dcc746a546fea7542d55dd801511555e
Tuantai2000 <72971837+Tuantai2000@users.noreply.github.com>
2022-06-21 15:30:13 +0700

    Cài đặt và cấu hình Prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import Button  from '~/component/button';
-import './App.css';
+import logo from "./logo.svg";
+import Button from "~/component/button";
+import "./App.css";
 
 function App() {
   return (
